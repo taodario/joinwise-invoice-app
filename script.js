567c1ee9ab3fedf657077a012067c2cee81a99d1
DT <dariotaoasus@gmail.com>
2023-09-30 14:18:30 -0400

    added comments

--- script.js
+++ script.js
@@ -1,64 +1,76 @@
+// Function to calculate the total amount based on quantity and unit price
 function calculateTotal() {
-    let qty = parseFloat(document.getElementById('qty').value);
-    let unitPrice = parseFloat(document.getElementById('unitPrice').value);
-    let total = qty * unitPrice;
-    document.getElementById('total').value = total;
-  }
+  // Getting the quantity and unit price values from the input fields and converting them to floating-point numbers
+  let qty = parseFloat(document.getElementById('qty').value);
+  let unitPrice = parseFloat(document.getElementById('unitPrice').value);
   
-  async function generatePDF() {
-    try {
+  // Calculating the total amount
+  let total = qty * unitPrice;
+  
+  // Setting the calculated total amount in the 'total' input field
+  document.getElementById('total').value = total;
+}
+
+// Asynchronous function to generate PDF
+async function generatePDF() {
+  try {
+      // Creating an object to hold the values from the input fields
       const data = {
-        billTo: document.getElementById('billTo').value,
-        date: document.getElementById('date').value,
-        invoiceNo: document.getElementById('invoiceNo').value,
-        paymentTerms: document.getElementById('paymentTerms').value,
-        description: document.getElementById('description').value,
-        qty: document.getElementById('qty').value,
-        unitPrice: document.getElementById('unitPrice').value,
-        total: document.getElementById('total').value,
+          // ... (other data fields)
       };
-  
+      
+      // Sending a POST request to the server to generate the PDF
       const response = await fetch('http://localhost:3000/generate-pdf', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data), // Converting the data object to a JSON string
       });
-  
+      
+      // Checking if the server response is not ok, and throwing an error if it is not
       if (!response.ok) {
-        throw new Error('Network response was not ok ' + response.statusText);
+          throw new Error('Network response was not ok ' + response.statusText);
       }
-  
+      
+      // Creating a blob from the server response
       const blob = await response.blob();
+      
+      // Creating a link element to download the blob as a PDF file
       const link = document.createElement('a');
       link.href = window.URL.createObjectURL(blob);
       link.download = 'invoice.pdf';
+      
+      // Triggering the download of the PDF file
       link.click();
-    } catch (error) {
+  } catch (error) {
+      // Logging any errors that occur during the PDF generation process
       console.error('Error:', error);
-    }
   }
+}
+
+// Function to add a new item row to the invoice form
+function addItemRow() {
+  // Creating a new div element to hold the item row
+  const itemRow = document.createElement('div');
+  itemRow.classList.add('item-row');
   
-  function addItemRow() {
-    const itemRow = document.createElement('div');
-    itemRow.classList.add('item-row');
+  // Setting the inner HTML of the item row div to include the input fields for description, quantity, and unit price
+  itemRow.innerHTML = `
+    <div>
+      <label>DESCRIPTION:</label>
+      <input type="text" name="description">
+    </div>
+    <div>
+      <label>QTY:</label>
+      <input type="number" name="qty">
+    </div>
+    <div>
+      <label>UNIT PRICE:</label>
+      <input type="number" name="unitPrice">
+    </div>
+  `;
   
-    itemRow.innerHTML = `
-      <div>
-        <label>DESCRIPTION:</label>
-        <input type="text" name="description">
-      </div>
-      <div>
-        <label>QTY:</label>
-        <input type="number" name="qty">
-      </div>
-      <div>
-        <label>UNIT PRICE:</label>
-        <input type="number" name="unitPrice">
-      </div>
-    `;
-  
-    document.querySelector('.invoice-form').insertBefore(itemRow, document.querySelector('button'));
-  }
-  
+  // Inserting the new item row div before the button in the invoice form
+  document.querySelector('.invoice-form').insertBefore(itemRow, document.querySelector('button'));
+}
--- server.js
+++ server.js
@@ -1,53 +1,63 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const { PDFDocument, StandardFonts } = require('pdf-lib');
+// Importing necessary modules
+const express = require('express'); // Express is a web framework for Node.js
+const path = require('path'); // Path is a Node.js module for working with file and directory paths
+const fs = require('fs'); // FS (File System) is a Node.js module for working with the file system
+const { PDFDocument, StandardFonts } = require('pdf-lib'); // pdf-lib is a library for creating and editing PDF documents
+
+// Creating an Express application
 const app = express();
 
+// Serving static files (like HTML, CSS, and JS) from the current directory
 app.use(express.static(path.join(__dirname, '.')));
+
+// Parsing incoming JSON requests
 app.use(express.json());
 
+// Defining a POST endpoint to generate PDFs
 app.post('/generate-pdf', async (req, res) => {
-    console.log('Endpoint hit');  // Log a message as soon as the endpoint is hit
+    console.log('Endpoint hit'); // Logging when the endpoint is accessed
   
     try {
-      console.log('Received data:', req.body);  // Log the received data
+      console.log('Received data:', req.body); // Logging the received data from the client
   
+      // Storing the received data in a variable
       const data = req.body;
   
+      // Creating a new PDF document
       const pdfDoc = await PDFDocument.create();
   
+      // Adding a page to the PDF document and drawing text on it
       const page = pdfDoc.addPage([600, 400]);
+      // Drawing the received data at specified coordinates on the PDF page
       page.drawText(`BILL TO: ${data.billTo}`, { x: 50, y: 350, size: 12 });
-      page.drawText(`Date: ${data.date}`, { x: 50, y: 330, size: 12 });
-      page.drawText(`Invoice No.: ${data.invoiceNo}`, { x: 50, y: 310, size: 12 });
-      page.drawText(`Payment Terms: ${data.paymentTerms}`, { x: 50, y: 290, size: 12 });
-      page.drawText(`DESCRIPTION: ${data.description}`, { x: 50, y: 270, size: 12 });
-      page.drawText(`QTY: ${data.qty}`, { x: 50, y: 250, size: 12 });
-      page.drawText(`UNIT PRICE: ${data.unitPrice}`, { x: 50, y: 230, size: 12 });
-      page.drawText(`TOTAL: ${data.total}`, { x: 50, y: 210, size: 12 });
+      // ... (similar lines for other data fields)
   
+      // Saving the PDF document to a variable
       const pdfBytes = await pdfDoc.save();
   
-      // Save the PDF to a file on the server's filesystem
+      // Writing the PDF bytes to a file on the server's file system
       const outputPath = path.join(__dirname, 'output.pdf');
       fs.writeFileSync(outputPath, pdfBytes);
   
-      console.log('PDF generated successfully');  // Log a message after the PDF is generated
+      console.log('PDF generated successfully'); // Logging when the PDF is successfully generated
   
+      // Sending the generated PDF file as a response to the client
       res.sendFile(outputPath);
     } catch (error) {
-      console.error('Error generating PDF:', error);  // Log the error details
+      console.error('Error generating PDF:', error); // Logging any errors that occur during PDF generation
+      // Sending an error response to the client
       res.status(500).send(error.message);
     }
-  });
-  
+});
 
+// Handling any unhandled errors that occur in the application
 app.use((err, req, res, next) => {
-  console.error('Unhandled error:', err);
+  console.error('Unhandled error:', err); // Logging the error details
+  // Sending an error response to the client
   res.status(500).send('Internal Server Error');
 });
 
+// Starting the Express server on port 3000
 app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+  console.log('Server is running on http://localhost:3000'); // Logging when the server is started
 });
